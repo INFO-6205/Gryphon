2dd20e55c80dc019d228fab37c0c393e5f6428b1
Robin Hillyard <r.hillyard@northeastern.edu>
2023-04-08 17:04:31 -0400

    Added VertexSpec

--- src/main/scala/com/phasmidsoftware/gryphon/core/Vertex.scala
+++ src/main/scala/com/phasmidsoftware/gryphon/core/Vertex.scala
@@ -6,7 +6,7 @@ package com.phasmidsoftware.gryphon.core
  * @tparam V the key (attribute) type of this Vertex.
  * @tparam X the "edge" type for the adjacent edges of this Vertex.
  */
-trait Vertex[V, X <: EdgeLike[V]] extends VertexLike[V] {
+trait Vertex[+V, X <: EdgeLike[V]] extends VertexLike[V] {
     /**
      * Method to add an edge (x) to this Vertex.
      *
@@ -51,6 +51,24 @@ abstract class AbstractVertex[V, X <: EdgeLike[V]] extends Vertex[V, X] {
      * @return a new AbstractVertex[V, Y].
      */
     def unit[Y <: EdgeLike[V]](adjacent: AdjacencyList[Y]): AbstractVertex[V, Y]
+}
+
+/**
+ * Case class to represent a concrete Vertex.
+ * NOTE: clients usually will not need to reference this class directly: instead use Vertex.empty.
+ *
+ * @param attribute (V) the attribute/key of the resulting Vertex.
+ * @param adjacent  (X) the adjacency list of the resulting Vertex.
+ * @tparam V the key (attribute) type of this Vertex.
+ * @tparam X the "edge" type for the adjacent edges of this Vertex (a sub-type of EdgeLike[V]).
+ */
+case class ConcreteVertex[V, X <: EdgeLike[V]](val attribute: V, val adjacent: AdjacencyList[X]) extends AbstractVertex[V, X] {
+    def unit[Y <: EdgeLike[V]](adjacent: AdjacencyList[Y]): AbstractVertex[V, Y] = ConcreteVertex[V, Y](attribute, adjacent)
+
+}
+
+object Vertex {
+    def empty[V, X <: EdgeLike[V]](a: V): Vertex[V, X] = ConcreteVertex[V, X](a, AdjacencyList.empty)
 }
 
 /**
---
+++ src/test/scala/com/phasmidsoftware/gryphon/core/VertexSpec.scala
@@ -0,0 +1,29 @@
+package com.phasmidsoftware.gryphon.core
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class VertexSpec extends AnyFlatSpec with should.Matchers {
+
+    behavior of "Vertex"
+
+    it should "attribute" in {
+        Vertex.empty("A").attribute shouldBe "A"
+    }
+
+    it should "degree" in {
+        Vertex.empty("A").degree shouldBe 0
+        Vertex.empty("A").addEdge(DirectedEdge("A", "B", "ab")).degree shouldBe 1
+    }
+
+    it should "adjacent" in {
+        Vertex.empty("A").adjacent shouldBe AdjacencyList.empty
+        Vertex.empty("A").addEdge(DirectedEdge("A", "B", "ab")).adjacent shouldBe AdjacencyList(Seq(DirectedEdge("A", "B", "ab")))
+    }
+
+    it should "addEdge" in {
+        val a = Vertex.empty[String, DirectedEdge[String, String]]("A")
+        a.addEdge(DirectedEdge("A", "B", "ab")) shouldBe new ConcreteVertex("A", AdjacencyList(Seq(DirectedEdge("A", "B", "ab"))))
+    }
+
+}
