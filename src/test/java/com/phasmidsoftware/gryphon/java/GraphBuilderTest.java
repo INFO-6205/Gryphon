882569cd6f74c302bffb56d117593bf716837689
Robin Hillyard <r.hillyard@northeastern.edu>
2023-04-19 14:35:23 -0400

    Implemented dfs for Java.
    Added VisitorJava;
    Now, VisitorJava class properly handles pre, post and reversePost.

--- src/test/java/com/phasmidsoftware/gryphon/java/GraphBuilderTest.java
+++ src/test/java/com/phasmidsoftware/gryphon/java/GraphBuilderTest.java
@@ -8,14 +8,18 @@ import scala.runtime.BoxedUnit;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.Assert.assertTrue;
+
 public class GraphBuilderTest {
 
     @Test
     public void createUndirectedEdgeList() {
         GraphBuilderJava<String, String, BoxedUnit> gb = GraphBuilderJava.create(w -> w, w -> w);
         Optional<List<UndirectedOrderedEdge<String, String>>> maybeEdges = gb.createUndirectedEdgeList("/prim.graph");
+        assertTrue(maybeEdges.isPresent());
         System.out.println(maybeEdges);
         Optional<Graph<String, String, UndirectedOrderedEdge<String, String>, BoxedUnit>> maybeGraph = gb.createGraphFromUndirectedEdgeList(maybeEdges);
+        assertTrue(maybeGraph.isPresent());
         System.out.println(maybeGraph);
     }
 }
